124c33450e96007e9be5026f2de347e0ad7ec561
TamiCardozo <cardozotamara53@gmail.com>
2026-03-17 18:03:04 -0300

    Actualización server.js para deploy en Render

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
-import dotenv from "dotenv";
-dotenv.config();
+import express from "express";
+import cors from "cors";
 
-import app from "./src/app.js";
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Backend corriendo en http://localhost:${PORT}`);
+//  rutas
+app.get("/api/products", (req, res) => {
+  res.json([/* tus productos */]);
 });
+
+// Puertos
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
